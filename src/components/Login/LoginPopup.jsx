ae4f9add2a891e8e1a5c31440272f1902833b2a9
haegi <b_haegi@naver.com>
2025-04-23 18:26:38 +0900

    login

--- src/components/Login/LoginPopup.jsx
+++ src/components/Login/LoginPopup.jsx
@@ -15,15 +15,9 @@ export default function LoginPopup({ onClose }) {
           >
             Kakao Login
           </button>
-          <button className="bg-blue-500 text-white px-4 py-2 rounded">
-            네이버로 로그인
-          </button>
-          <button className="bg-gray-300 text-black px-4 py-2 rounded">
-            애플로 로그인
-          </button>
         </div>
         <button className="mt-4 text-red-500" onClick={onClose}>
-          닫기
+          close
         </button>
       </div>
     </div>
